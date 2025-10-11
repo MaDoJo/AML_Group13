fb29f16d2f4d7e5f5500e38930a4a0f07365e285
RobEverts <r.everts.4@student.rug.nl>
2025-10-11 15:46:55 +0200

    formatted and documented DTW functions

--- DynamicTimeWarping.py
+++ DynamicTimeWarping.py
@@ -1,16 +1,39 @@
 import numpy as np
 
-from loadData import MAX_LENGTH
-
-N_CHANNELS = 12
+from loadData import MAX_LENGTH, N_CHANNELS
 
 def format_signal(signal: np.ndarray) -> np.ndarray:
+    """
+    Removes the padded values from a signal
+
+    Args:
+        signal (np.ndarray): a (padded) time series with 12 channels of  
+        cepstrum coefficients.
+
+    Returns:
+        np.ndarray: the same signal as the input signal, but without the 
+        padded values (0's).
+    """
+
     last_row = np.nonzero(signal)[0][-1] + 1        # last index of non-padded row
     signal = np.array([signal[:last_row, channel] for channel in range(N_CHANNELS)])
     return signal
 
 
 def get_distance_matrix(signal1: np.ndarray, signal2: np.ndarray) -> np.ndarray:
+    """
+    Generates the distance matrix required for the Dynamic Time Warping (DTW)
+    procedure.
+
+    Args:
+        signal1 (np.ndarray): first signal used in the distance matrix
+        signal2 (np.ndarray): second signal used in the distance matrix.
+
+    Returns:
+        np.ndarray: the distance matrix with shape (length signal 1, length 
+        signal 2).
+    """
+
     # initialize distance-matrix
     distance_matrix = np.zeros((signal1.shape[0], signal2.shape[0]))
 
@@ -36,32 +59,60 @@ def get_distance_matrix(signal1: np.ndarray, signal2: np.ndarray) -> np.ndarray:
     return distance_matrix
 
 def get_distance(distance_matrix: np.ndarray) -> float:
+    """
+    Calculates the distance according to the Dynamic Time Warping (DTW)
+    procedure, given a distance matrix.
+
+    Args:
+        distance_matrix (np.ndarray): a distance matrix used for the DTW
+        procedure.
+
+    Returns:
+        float: the accumulated distance from following the path on the
+        distance matrix, according to the DTW procedure.
+    """
+
     distance = 0
+    # start at the last row, last column index of the distance matrix
     idx = (distance_matrix.shape[0] - 1, distance_matrix.shape[1] - 1)
 
+    # while not at the first row, first column index of the distance matrix
     while idx != (0, 0):
         distance += distance_matrix[idx[0], idx[1]]
 
-        connections = np.array([])
+        # select the possible indexes to move to and the values of these indexes
+        # in the distance matrix
+        connections = []
         idxs = []
         if idx[0] > 0:
             idxs.append((idx[0] - 1, idx[1]))
-            connections = np.append(connections, distance_matrix[idx[0] - 1, idx[1]])
+            connections.append(connections, distance_matrix[idx[0] - 1, idx[1]])
         if idx[1] > 0:
             idxs.append((idx[0], idx[1] - 1))
-            connections = np.append(connections, distance_matrix[idx[0], idx[1] - 1])
+            connections.append(distance_matrix[idx[0], idx[1] - 1])
         if idx[0] > 0 and idx[1] > 0:
             idxs.append((idx[0] - 1, idx[1] - 1))
-            connections = np.append(connections, distance_matrix[idx[0] - 1, idx[1] - 1])
-        
-        idx = idxs[np.argmin(connections)]
+            connections.append(distance_matrix[idx[0] - 1, idx[1] - 1])
+
+        # select the index with the lowest corresponding value
+        idx = idxs[np.argmin(np.array(connections))]
 
     return distance
 
 
-# Dynamic Time Warp function, meant as a measure of distance between
-# speach signals. Custom to our dataset
 def DTW(signal1: np.ndarray, signal2: np.ndarray) -> float:
+    """
+    Calculates the summed Dynamic Time Warping (DTW) distances over all
+    channels in the given signals.
+
+    Args:
+        signal1 (np.ndarray): first signal used to compare DTW distance with.
+        signal2 (np.ndarray): second signal used to compare DTW distance with.
+
+    Returns:
+        float: summed DTW distances over all 12 channels in the signals.
+    """
+
     if signal1.shape != (MAX_LENGTH, N_CHANNELS):
         print(f"expected second signal to have {MAX_LENGTH} time steps and \
               {N_CHANNELS} channels, got {signal1.shape[0]} time steps and \
@@ -73,10 +124,12 @@ def DTW(signal1: np.ndarray, signal2: np.ndarray) -> float:
               {N_CHANNELS} channels, got {signal2.shape[0]} time steps and \
                 {signal2.shape[1]} channels instead.")
         return
-    
+
+    # strip the padded values from the signals
     signal1 = format_signal(signal1)
     signal2 = format_signal(signal2)
 
+    # sum the DTW distances over all channels
     total_distance = 0
     for channel in range(N_CHANNELS):
         distance_matrix = get_distance_matrix(signal1[channel], signal2[channel])
--- loadData.py
+++ loadData.py
@@ -3,15 +3,16 @@ import os
 
 TRAIN_DATA_POINTS = 270
 TEST_DATA_POINTS = 370
+N_CHANNELS = 12
 N_CLASSES = 9
 MAX_LENGTH = 29
 
 def load_data(file_path: str, num_data_points: int, padding_value: float=0.0) -> np.ndarray:
     """
     Loads the data from the data files, splits it into num_data_points individual 
-    data points, (which are time series with 12 channels of cepstrum coefficients), 
-    pads them to the maximum sequence length (MAX_LENGTH), and returns a 3D NumPy 
-    array of shape (num_data_points, MAX_LENGTH, 12).
+    data points, (which are time series with N_CHANNELS channels of cepstrum 
+    coefficients), pads them to the maximum sequence length (MAX_LENGTH), and 
+    returns a 3D NumPy array of shape (num_data_points, MAX_LENGTH, N_CHANNELS).
 
     Args:
         file_path (str): The path to the data file.
@@ -20,7 +21,7 @@ def load_data(file_path: str, num_data_points: int, padding_value: float=0.0) ->
 
     Returns:
         numpy.ndarray: The data from the data file as a NumPy array of shape 
-        (num_data_points, MAX_LENGTH, 12).
+        (num_data_points, MAX_LENGTH, N_CHANNELS).
     """
 
     if not os.path.exists(file_path):
@@ -29,7 +30,6 @@ def load_data(file_path: str, num_data_points: int, padding_value: float=0.0) ->
 
     # load all data from the file
     full_data = np.loadtxt(file_path)
-    N_DIMENSIONS = full_data.shape[1]   # Should be 12
 
     # separator rows consist of all ones
     is_separator = np.all(full_data == 1.0, axis=1)
@@ -57,7 +57,7 @@ def load_data(file_path: str, num_data_points: int, padding_value: float=0.0) ->
     for arr in data_points:
         # create a padding array of the required size
         n_rows_to_pad = MAX_LENGTH - arr.shape[0]
-        padding = np.full((n_rows_to_pad, N_DIMENSIONS), padding_value, dtype=arr.dtype)
+        padding = np.full((n_rows_to_pad, N_CHANNELS), padding_value, dtype=arr.dtype)
 
         # add the padding to the the original array
         padded_arr = np.vstack([arr, padding])
